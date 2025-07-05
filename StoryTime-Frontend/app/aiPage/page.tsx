a323a513c5ddf32a952ac6fb7c28cf92aa5d9bca
Pravin Raju <tmpravinraju@gmail.com>
2025-07-05 09:58:48 +0530

    lazy loading added

--- StoryTime-Frontend/app/aiPage/page.tsx
+++ StoryTime-Frontend/app/aiPage/page.tsx
@@ -5,8 +5,16 @@ import AiPageClient from "@/components/AiPageClient";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div className="p-10 text-center">Loading AI Page...</div>}>
-      <AiPageClient />
-    </Suspense>
+    <Suspense
+  fallback={
+    <div className="p-10 flex flex-col items-center justify-center gap-4 text-white">
+      <div className="w-10 h-10 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
+      <div className="text-lg">Loading AI Page...</div>
+    </div>
+  }
+>
+  <AiPageClient />
+</Suspense>
+
   );
 }
