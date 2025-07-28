a7e064aa3a5a5430660f47e046865fcb74d3fb26
Pravin Raju <tmpravinraju@gmail.com>
2025-07-28 20:28:58 +0530

    made changes in auth route.js

--- StoryTime-Frontend/next.config.ts
+++ StoryTime-Frontend/next.config.ts
@@ -1,12 +1,22 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- 
+ async redirects() {
+    return [
+      {
+        source: '/api/auth/providers',
+        destination: '/api/auth/providers/',
+        permanent: true,
+      },
+    ]
+  },
   //basePath: "/StoryTime-Frontend", 
   trailingSlash: true,
    images: {
     domains: ['res.cloudinary.com', 'cdn-icons-png.flaticon.com','lh3.googleusercontent.com'],
   },
 };
+
+
 
 export default nextConfig;
